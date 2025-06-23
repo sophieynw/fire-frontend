0d60891ede6044b18d79fe06260e96328d97e871
Sophie Wang <wang.sophie@live.ca>
2025-06-22 23:51:29 -0400

    initial commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -20,7 +20,7 @@ function Form({ setPrediction }) {
     // console.log(payload);
 
     // fetch api
-    fetch("https://fire-render.onrender.com/api", {
+    fetch(API_URL, {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
